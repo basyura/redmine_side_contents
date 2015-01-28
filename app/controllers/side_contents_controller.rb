a529337813125b64e630e7c3ec6ba472a9e7a942
basyura <basyura@gmail.com>
2015-01-28 23:37:23 +0900

    refactored

--- app/controllers/side_contents_controller.rb
+++ app/controllers/side_contents_controller.rb
@@ -2,18 +2,16 @@ class SideContentsController < ApplicationController
   unloadable
 
   def edit
-    @project_id   = params[:project_id]
-    @common_text  = SideContent.find_text(0)
-    @project_text = SideContent.find_text(params[:project_id])
-
-    respond_to do |format|
-      format.js
-    end
+    render_view
   end
 
   def save
     SideContent.save_text(params)
+    render_view
+  end
 
+  private
+  def render_view
     @project      = Project.find(params[:project_id])
     @common_text  = SideContent.find_text(0)
     @project_text = SideContent.find_text(params[:project_id])
