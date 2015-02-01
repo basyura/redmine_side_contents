18ae87d72a75b229d480a83efc84a87be2a2ec7f
basyura <basyura@gmail.com>
2015-02-01 10:35:58 +0900

    improved readme

--- init.rb
+++ init.rb
@@ -3,8 +3,8 @@ require File.dirname(__FILE__) + '/lib/redmine_side_contents_hook'
 Redmine::Plugin.register :redmine_side_contents do
   name 'Redmine Side Contents plugin'
   author 'basyura'
-  description 'This is a plugin for Redmine'
+  description 'This is a editable sidebar plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
+  url 'https://github.com/basyura/redmine_side_contents'
   author_url 'basyura.org'
 end
